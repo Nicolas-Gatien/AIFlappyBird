36d129b23d2e600d68d44f25691775ebad86787b
polarcube1 <polarcube1@gmail.com>
2022-01-17 20:24:38 -0500

    Added Pipe
    Added:
    - Pipe Class in flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -96,6 +96,59 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
+# pipe class
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    # defining instance specific variables
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+    
+    # setting the top and bottom of the pipe
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+    
+    # moving the pipe
+    def move(self):
+        self.x -= self.VEL
+    
+    # drawing the pipe
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+    
+    def collide(self, bird):
+        # setting up pixel perfect collisions
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask  = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        botttom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        # checking overlap in pixels
+        b_point = bird_mask.overlap(bottom_mask, botttom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point:
+            return True
+        
+        return False
+
+        
+
 # drawing things on screen
 def draw_window(win, bird):
     win.blit(BG_IMG, (0,0))
